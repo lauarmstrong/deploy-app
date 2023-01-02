7d616a893ac001f9042d52aa04ad5e54dccb4b33
Lauren Armstrong <lauren.armstrong@vaultplatform.com>
2023-01-02 23:35:51 +0000

    update pipeline build

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -4,8 +4,9 @@
 
 export const environment = {
   production: true,
-  appName: "Udagram",
-  apiHost: "http://localhost:8081/api/v0",
+  appName: "udagram-api",
+  apiHost:
+    "http://udagram-api-dev2222222222.us-east-1.elasticbeanstalk.com/api/v0",
 };
 
 /*
